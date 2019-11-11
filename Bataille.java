e2dab3a05c374001a8d533cee8857a4b0af68eb8
Matx99 <44765946+Matx99@users.noreply.github.com>
2019-11-11 08:43:31 +0100

    Delete Bataille.java

--- Bataille.java
+++
@@ -1,9 +0,0 @@
-package fr.mat.bataille;
-
-public class Bataille {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Carte carte1 = new Carte("rouge", "roi");
-		System.out.println(carte1.couleur);
-	}
-}
