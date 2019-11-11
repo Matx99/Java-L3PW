226798524f9a08a518062df38053798d3aadf763
Matx99 <44765946+Matx99@users.noreply.github.com>
2019-11-11 08:41:49 +0100

    Add files via upload

---
+++ Bataille.java
@@ -0,0 +1,9 @@
+package fr.mat.bataille;
+
+public class Bataille {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Carte carte1 = new Carte("rouge", "roi");
+		System.out.println(carte1.couleur);
+	}
+}
