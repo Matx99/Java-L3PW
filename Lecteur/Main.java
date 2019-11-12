61d1fb5fe291fd3d1f86a0ed6aa305813e9eda0f
Matx99 <44765946+Matx99@users.noreply.github.com>
2019-11-12 13:36:32 +0100

    Update Main.java

--- Lecteur/Main.java
+++ Lecteur/Main.java
@@ -1,11 +1,11 @@
 class Main {
   public static void main(String[] args) {
-    // CrÃ©e un objet de type Txt
+    // Créé un objet de type Txt
     Fichiers txt = new Fichiers("toRead.txt");
     System.out.println(txt.lire());
     System.out.println(txt.lireInverse());
 
-    // CrÃ©e un objet de type CSV
+    // Créé un objet de type CSV
     Fichiers csv = new Fichiers("test.csv");
     System.out.println(csv.lire());
     System.out.println(csv.lireInverse());
